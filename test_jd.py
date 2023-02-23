46c15d697aaa4b4ea6305b1bd7ffb638482200ba
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-23 15:13:34 -0500

    u

--- test_jd.py
+++ test_jd.py
@@ -8,8 +8,8 @@ can_jd = CANJD(port, id)
 
 
 while True:
-    r = can_jd.get_rot_grav()
-    print(np.round(r.T,2), np.linalg.norm(r))
+    r = can_jd.get_accel()
+    print(np.round(r.T,2),np.linalg.norm(r))
     #print(round(x,2), round(y,2), can_jd.slope_resolution)
 
 
