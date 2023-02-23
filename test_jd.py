6812ddb45450a2e48da37966296b59b07c8c4f58
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-23 14:41:46 -0500

    u

--- test_jd.py
+++ test_jd.py
@@ -1,12 +1,33 @@
 from can_jd import *
+import numpy as np
 
 port = 'can1'
 id = 10
 
 can_jd = CANJD(port, id)
 
+g_vector = np.array([0, 0, -9.81]).T
+
+def rotation(thetax_deg, thetay_deg, g_vector):
+    thetax = thetax_deg*np.pi/180
+    Rx = np.array[[1, 0, 0],
+                  [0, np.cos(thetax), -np.sin(thetax)],
+                  [0, np.sin(thetax), np.cos(thetax)]]
+
+    thetay = thetay_deg*np.pi/180
+    Ry = np.array[[np.cos(thetay), 0, np.sin(thetay)],
+                  [0, 1, 0],
+                  [-np.sin(thetay), 0, np.cos(thetay)]]
+
+    g_rotated = Ry@Rx@g_vector
+    return g_rotated
+
 while True:
     x,y = can_jd.get_slopes()
-    print(round(x,2), round(y,2), can_jd.slope_resolution)
+
+    g_rot = rotation(x,y,g_vector)
+
+    print(g_rot.T)
+    #print(round(x,2), round(y,2), can_jd.slope_resolution)
 
 
