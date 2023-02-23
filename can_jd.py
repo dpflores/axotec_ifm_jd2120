405847ed4a9e23894bf65d68b681f01acecae960
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-23 14:58:14 -0500

    U

--- can_jd.py
+++ can_jd.py
@@ -19,11 +19,19 @@ class CANJD():
         self.slope_resolution = slope_resolutions[str(self.node.sdo[0x6000].raw)]
         
 
-    def get_accel(self):
+    def get_prop_accel(self):
         x = self.node.sdo[0x3403].raw * accel_resolution
         y = self.node.sdo[0x3404].raw * accel_resolution
         z = self.node.sdo[0x3405].raw * accel_resolution
         return x,y,z
+
+    def get_prop_accel_vector(self):
+        x = self.node.sdo[0x3403].raw * accel_resolution
+        y = self.node.sdo[0x3404].raw * accel_resolution
+        z = self.node.sdo[0x3405].raw * accel_resolution
+
+        f  = np.array([x,y,z]).T
+        return f
 
     def get_gyro(self):
         x = self.node.sdo[0x3400].raw * gyro_resolution 
@@ -35,6 +43,31 @@ class CANJD():
         x = self.node.sdo[0x6010].raw * self.slope_resolution
         y = self.node.sdo[0x6020].raw * self.slope_resolution
         return x,y
+
+    def get_rot_grav(self):
+        ''' Rotated gravity '''
+        thetax_deg, thetay_deg = self.get_slopes()
+        thetax = -thetax_deg*np.pi/180
+        Rx = np.array([[1, 0, 0],
+                    [0, np.cos(thetax), -np.sin(thetax)],
+                    [0, np.sin(thetax), np.cos(thetax)]])
+
+        thetay = thetay_deg*np.pi/180
+        Ry = np.array([[np.cos(thetay), 0, np.sin(thetay)],
+                    [0, 1, 0],
+                    [-np.sin(thetay), 0, np.cos(thetay)]])
+
+        g_rotated = Ry@Rx@g_vector
+        return g_rotated
+
+    def get_accel(self):
+        ''' Retorna la aceleracion respecto al eje fijo, descontando el efecto de la gravedad
+        en la aceleracion propia (proper acceleration)'''
+        f = self.get_prop_accel_vector()
+        g = self.get_rot_grav()
+        r = f + g
+        return r 
+
 
 
 
